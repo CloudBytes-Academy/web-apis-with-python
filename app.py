c94707f5282abda1ce7b62a15e8512103451110c
justgoodin <rehan.haider.v3@gmail.com>
2021-06-04 15:39:49 +0000

    P1: Hello API using Flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+
+# Intitialise the app
+app = Flask(__name__)
+
+# Define what the app does
+@app.route("/greet")
+def index():
+    """
+    TODO:
+    1. Capture first name & last name
+    2. If either is not provided: respond with an error
+    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
+    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
+    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
+    """
+    return jsonify("TODO")
