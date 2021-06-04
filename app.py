2e69e474b1fd554d8d60072101b9d68f2061f6b4
justgoodin <rehan.haider.v3@gmail.com>
2021-06-04 15:54:42 +0000

    Problem 2

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    """
+    TODO: Render the home page provided under templates/index.html in the repository
+    """
+    return "TODO"
+
+@app.get("/search")
+def search():
+	"""
+	TODO:
+	1. Capture the word that is being searched
+	2. Seach for the word on Google and display results
+	"""
+    return "TODO"
+
+if __name__ == "__main__":
+    app.run()
