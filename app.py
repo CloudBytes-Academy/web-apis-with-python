5223a1ade4f36cc1e6e1dd412e471e4e49154bc2
justgoodin <rehan.haider.v3@gmail.com>
2021-06-05 06:15:19 +0000

    Starter Kit

---
+++ app.py
@@ -0,0 +1,33 @@
+# dictionary-api-python-flask/app.py
+from flask import Flask, request, jsonify, render_template
+from model.dbHandler import match_exact, match_like
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Provide usage instructions formatted as JSON
+    """
+    # response = {"usage": "/dict?=<word>"}
+    # Since this is a website with front-end, we don't need to send the usage instructions
+    return "TODO"
+
+
+@app.get("/dict")
+def dictionary():
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Accept a word from the request
+    2. Try to find an exact match, and return it if found
+    3. If not found, find all approximate matches and return
+    """
+    return "TODO"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ model/dbHandler.py
@@ -0,0 +1,39 @@
+import sqlite3 as SQL
+
+
+def match_exact(word: str) -> list:
+    """
+    This method will:
+    1. Accept a string
+    2. Search the dictionary for an exact match
+    3. If success return the definition
+    4. If not return an empty list
+    """
+
+    # TODO: Establish connection to the dictionary database
+
+    # TODO: Query the database for exact matches
+
+    # TODO: Clone the connection to the database
+    
+
+    # Return the results
+    return "TODO"
+
+
+def match_like(word: str) -> list:
+    """
+    This method will:
+    1. Accept a string
+    2. Search the dictionary for approximate matches
+    3. If success return the definition as a list
+    4. If not return an empty list
+    """
+    # TODO: Establish connection to the dictionary database
+    
+    # TODO: Query the database for exact matches
+    
+    # TODO: Clone the connection to the database
+    
+    # Return the results
+    return "TODO"
