a9264e436cc8dfb67a50e86bb715cd2d27b1e66e
justgoodin <rehan.haider.v3@gmail.com>
2021-06-06 09:02:45 +0000

    P6 Starter Kit

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+from typing import Optional
 from fastapi import FastAPI
+from model.dbHandler import match_exact, match_like
 
-app = FastAPI()
+app = Flask(__name__)
 
 
 @app.get("/")
 def index():
-    return {"hello": "FastAPI"}
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Provide usage instructions formatted as JSON
+    """
+    return "TODO"
+
+
+@app.get("/dict")
+def dictionary():
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Accept a word from the request
+    2. Try to find an exact match, and return it if found
+    3. If not found, find all approximate matches and return
+    """
+    return "TODO"
