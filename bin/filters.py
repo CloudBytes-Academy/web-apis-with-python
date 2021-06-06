8f322e66f7382911b6ad34712b011feae1f9f5b6
justgoodin <rehan.haider.v3@gmail.com>
2021-06-06 19:35:18 +0000

    S7: Solution

---
+++ bin/filters.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageFilter
+import io
+
+
+def apply_filter(file: object, filter: str) -> object:
+    """
+    TODO:
+    1. Accept the image as file object, and the filter type as string
+    2. Open the as an PIL Image object
+    3. Apply the filter
+    4. Convert the PIL Image object to file object
+    5. Return the file object
+    """
+    image = Image.open(file)
+    image = image.filter(eval(f"ImageFilter.{filter.upper()}"))
+
+    file = io.BytesIO()
+    image.save(file, "JPEG")
+    file.seek(0)
+
+    return file
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from bin.filters import apply_filter
 from typing import List, Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, File, UploadFile
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import StreamingResponse
+import io
 
 
 app = FastAPI()
@@ -32,3 +35,21 @@ def index():
         "usage": {"http_method": "POST", "URL": "/<filter_available>/"},
     }
     return jsonable_encoder(response)
+
+
+@app.post("/{filter}")
+def image_filter(filter: str, img: UploadFile = File(...)):
+    """
+    TODO:
+    1. Checks if the provided filter is available, if not, return an error
+    2. Check if a file has been provided in the POST request, if not return an error
+    3. Apply the filter using apply_filter() method from bin.filters
+    4. Return the filtered image as response
+    """
+    if filter not in filters_available:
+        response = {"error": "incorrect filter"}
+        return jsonable_encoder(response)
+
+    filtered_image = apply_filter(img.file, filter)
+
+    return StreamingResponse(filtered_image, media_type="image/jpeg")
