5f1c5f5707321cddf30f3b0e4bf1dfc5692b5668
Max Ilin <mgilyin@gmail.com>
2022-07-17 13:03:33 +0300

    refactoring data

--- app.py
+++ app.py
@@ -5,17 +5,8 @@ from flask import render_template
 # Intitialise the app
 app = Flask(__name__)
 
-# Define what the app does
-@app.get("/greet")
-def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
+
+def return_message():
     fname = request.args.get("fname")
     lname = request.args.get("lname")
     if not fname and not lname:
@@ -26,21 +17,16 @@ def index():
         response = {'data': f"Hello, Mr {lname}"}
     else:
         response = {"data": f"Is your name {fname} {lname}?"}
-    
+    return response
+
+# Define what the app does
+@app.get("/greet")
+def index():
+    response = return_message()
     return jsonify(response)
 
 
 @app.get("/hello")
 def hello():
-    fname = request.args.get("fname")
-    lname = request.args.get("lname")
-    if not fname and not lname:
-        return jsonify({"status": "error"})
-    elif fname and not lname:
-        response = {"data": f"Hello, {fname}!"}
-    elif not fname and lname:
-        response = {'data': f"Hello, Mr {lname}"}
-    else:
-        response = {"data": f"Is your name {fname} {lname}?"}
-
+    response = return_message()
     return render_template("hello.html", data = response["data"])
