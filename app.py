d7f3e489ef45debfbdd7ecd03e3b03e9dcd67407
Tigran Mirvelyan <tabcd@mail.ru>
2022-08-12 20:57:55 +0400

    everything done

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from bin.filters import apply_filter
 app = Flask(__name__)
 
 # Read the PIL document to find out which filters are available out-of the box
-filters_available = []
+filters_available = [
+ "blur" ,
+ "contour" ,
+ "detail" ,
+ "edge_enhance" ,
+ "edge_enhance_more" ,
+ "emboss" ,
+ "find_edges" ,
+ "sharpen" ,
+ "smooth" ,
+ "smooth_more",]
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -13,11 +23,29 @@ def index():
     TODO:
     1. Return the usage instructions that specifies which filters are available, and the method format
     """
-    pass
+    response = {
+                "filters_available" : filters_available,
+                "usage" : { "http_method" : "POST" , "URL" : "/<filter_available>/" },
+               }
+    return jsonify(response)
 
 
 @app.post("/<filter>")
 def image_filter(filter):
+    print(f"filter is {filter}")
+    print( filter not in filters_available)
+    if filter not in filters_available:
+        print("0000")
+        return jsonify({"status":"error here no filter you want"})
+    print("1")
+    file = request.files["image"]
+    if not file:
+        return jsonify({"status":"error missing file"})
+    print("2")
+    filtered_image = apply_filter(file,filter)
+    print("3")
+    return send_file(filtered_image,mimetype="image/JPEG")
+
     """
     TODO:
     1. Checks if the provided filter is available, if not, return an error
@@ -25,9 +53,7 @@ def image_filter(filter):
     3. Apply the filter using apply_filter() method from bin.filters
     4. Return the filtered image as response
     """
-    pass
-
-
+    
 if __name__ == "__main__":
     app.run()
     
