6750eddcd7c82eacd60864bffa3643c3f228201e
M-Bash <bashim7731@gmail.com>
2022-09-30 13:24:04 +0100

    updated contents of app.py

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from flask import Flask, request, render_template, redirect
+import random
 
 app = Flask(__name__)
+
 
 @app.get("/")
 def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
+    return render_template("index.html")
+
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+   args=request.args.get("q")
+   return redirect(f"https://google.com/search?q={args}")
+
+@app.get("/lucky")
+def lucky():
+    with open("textFile.txt", "r") as file:
+        text = file.read()
+        words = list(map(str, text.split()))
+        arg= random.choice(words)
+        return redirect(f"https://google.com/search?q={arg}")
+	
 
 if __name__ == "__main__":
     app.run()
