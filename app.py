1a7445b8647f5f5f4c760fa9d9ed9ae497c53f58
apin014 <davindzimar@gmail.com>
2022-11-10 18:18:57 +0700

    proceeded to ch 5 in book

--- app.py
+++ app.py
@@ -14,4 +14,17 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+    
+    if not fname and not lname:
+        return jsonify({"status":"error"})
+    elif fname and not lname:
+        response = {"data":f"Hello, {fname}!"}
+    elif not fname and lname:
+        response = {"data":f"Hello, Mr. {lname}!"}    
+    else:
+        response = {"data":f"Is your name {fname} {lname}?"}
+        
+    return jsonify(response)
