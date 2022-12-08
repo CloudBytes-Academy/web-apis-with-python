678c0402ad65fa2c69f7e80395b7a7f9a393f76f
LUKASIEWICZ\serhii.baraban <serhii.baraban@pit.lukasiewicz.gov.pl>
2022-12-08 09:02:41 +0100

    Realized TODO comment

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    response = {"data":"Hello, World!"}
-    return jsonify(response)
+    name = input("Tell me your first name:")
+    surname = input("Tell me your second name:")
+    if name == '' and surname == '':
+        return "Please, provide first and last name" # ValueError: invalid
+    elif name == '':
+        return "Hello Mr {}!".format(surname)
+    elif surname == '':
+        return "Hello, {}!".format(name)
+    else:
+        return "Is your name {} {}".format(name, surname)
