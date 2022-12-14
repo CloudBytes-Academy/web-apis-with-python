8def94b447a0aead5f2e5cf878e895370aedbac0
LUKASIEWICZ\serhii.baraban <serhii.baraban@pit.lukasiewicz.gov.pl>
2022-12-14 12:04:03 +0100

    Building a dictionary API

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    # response = {"usage": "/dict?=<word>"}
+    response = {"usage":"/dict?=<word>"}
     # Since this is a website with front-end, we don't need to send the usage instructions
-    return "TODO"
+    return jsonify(response)
 
 
 @app.get("/dict")
@@ -26,8 +26,26 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+    words = request.args.getlist("word")
 
+    if not words:
+        response = {"status":"error", "word":words, "data":"word not found"}
+        return jsonify(response)
+
+    response = {"words":[]}
+    
+    for word in words:
+        definitions = match_exact(word)
+        if definitions:
+            response["words"].append({"status":"success","word":word,"data":definitions})
+        else:
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({"status":"partial","word":word,"data":definitions})
+            else:
+                response["words"].append({"status":"error","word":word,"data":"word not found"})
+
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
--- model/dbHandler.py
+++ model/dbHandler.py
@@ -11,14 +11,14 @@ def match_exact(word: str) -> list:
     """
 
     # TODO: Establish connection to the dictionary database
-
+    db = SQL.connect("data/dictionary.db")
     # TODO: Query the database for exact matches
-
+    sql_query = "SELECT*from entries WHERE word=?"
+    match = db.execute(sql_query, (word,)).fetchall()
     # TODO: Clone the connection to the database
-    
-
+    db.close()
     # Return the results
-    return "TODO"
+    return match
 
 
 def match_like(word: str) -> list:
@@ -30,10 +30,11 @@ def match_like(word: str) -> list:
     4. If not return an empty list
     """
     # TODO: Establish connection to the dictionary database
-    
+    db = SQL.connect("data/dictionary.db")
     # TODO: Query the database for exact matches
-    
+    sql_query = "SELECT*from entries WHERE word LIKE ?"
+    match = db.execute(sql_query, ("%" + word + "%",)).fetchall()
     # TODO: Clone the connection to the database
-    
+    db.close()
     # Return the results
-    return "TODO"
+    return match
