734ad72169c0db2f3e589b619dee6e4bd2841502
LUKASIEWICZ\serhii.baraban <serhii.baraban@pit.lukasiewicz.gov.pl>
2022-12-21 08:53:05 +0100

    Built the POST API

--- bin/filters.py
+++ bin/filters.py
@@ -11,5 +11,10 @@ def apply_filter(file: object, filter: str) -> object:
     4. Convert the PIL Image object to file object
     5. Return the file object
     """
+    image = Image.open(file)
+    image = image.filter(eval(f"ImageFilter.{filter.upper()}"))
+    file = io.BytesIO()
+    image.save(file, "JPEG")
+    file.seek(0)
 
-    pass
+    return file
