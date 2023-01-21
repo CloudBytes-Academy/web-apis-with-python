67f94272f77b273637e210fc50cc4bc44a7ac2c8
cinv-e <123252630+cinv-e@users.noreply.github.com>
2023-01-22 00:22:12 +0100

    implemented redirection and fixed 404 error

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
-
+    return render_template("index.html")
+    
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    """
+    TODO:
+    1. Capture the word that is being searched
+    2. Seach for the word on Google and display results
+    """
+    args=request.args.get("q")
+    return redirect(f"https://www.google.com/search?q= {args}")
 
 if __name__ == "__main__":
     app.run()
